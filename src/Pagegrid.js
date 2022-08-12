866cbb3eb11501bff14f157543212ab2c9f5ec1d
Ajithkumar1810 <jaganajith18@gmail.com>
2022-08-12 17:08:37 +0530

    4rd commit

--- src/Pagegrid.js
+++ src/Pagegrid.js
@@ -21,7 +21,7 @@ function Pagegrid(){
     .map((user)=>{
         return (
             <div className="user">
-                        <h3>{user.firstname}</h3>
+                        {/* <h3>{user.firstname}</h3>
                         <h3>{user.lastname}</h3>
                         <h3>{user.dob}</h3>
                         <h3>{user.email}</h3>
@@ -30,15 +30,45 @@ function Pagegrid(){
                         <h3>{user.state}</h3>
                         <h3>{user.city}</h3>
                         <h3>{user.address}</h3>
-                        <h3>{user.pincode}</h3>
-
+                        <h3>{user.pincode}</h3> */}
+                <table >
+                    <tr>
+                        <td><input type="checkbox"></input></td>
+                        <td>{user.firstname}</td>
+                        <td>{user.lastname}</td>
+                        <td>{user.dob}</td>
+                        <td id="email">{user.email}</td>
+                        <td>{user.gender}</td>
+                        <td>{user.country}</td>
+                        <td>{user.state}</td>
+                        <td>{user.city}</td>
+                        <td>{user.address}</td>
+                        <td>{user.pincode}</td>
+                    </tr>
+                </table>
                     </div>
         );
     })
     
     return (
         <div className="Pagegrid">
-            {displayUser}
+            
+            <table id="title">
+                    <tr>
+            <td><input type="checkbox"></input></td>
+                        <td>FirstName</td>
+                        <td>LastName</td>
+                        <td>Date of birth</td>
+                        <td>Email</td>
+                        <td>Gender</td>
+                        <td>Country</td>
+                        <td>State</td>
+                        <td>City</td>
+                        <td>Address</td>
+                        <td>Pincode</td>
+                        </tr>
+                </table>
+                {displayUser}
             <ReactPaginate
                 previousLabel={"Previous"}
                 nextLabel={"Next"}
