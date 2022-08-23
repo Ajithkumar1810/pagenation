6dff176b6f899e3cd77aac8b8faea65d88590e9d
Ajithkumar1810 <jaganajith18@gmail.com>
2022-08-23 10:41:30 +0530

    adding value done

--- src/Dataentry.js
+++ src/Dataentry.js
@@ -1,7 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './Dataentry.css';
-// import { BsFillTrashFill } from "react-icons/bs";
-
 import React,{useState} from "react";
 import JsonData from "./Mock_data.json";
 import ReactPaginate from "react-paginate";
@@ -11,11 +9,17 @@ import { BsFillPencilFill } from "react-icons/bs";
 import './Dataentry.js';
 function Dataentry() {
     
-    let users1=JsonData;
-   
-        users1=[...users1].sort((a,b)=>a.firstname>b.firstname?1:-1,);
-    
-        
+    let [users1]=[JsonData.sort((a,b)=>a.firstname>b.firstname?1:-1,)];
+    const [FirstNameDOM,setFirstNameDOM]=React.useState('');
+      const [LastnameDOM,setLastnameDOM]=React.useState('');
+      const [DateDOM,setDateDOM]=React.useState('');
+      const [EmailDOM,setEmailDOM]=React.useState('');
+      const [MaleDOM]=React.useState('Male');
+      const [CountryDOM,setCountryDOM]=React.useState('');
+      const [StateDOM,serStateDOM]=React.useState('');
+      const [CityDOM,setCityDOM]=React.useState('');
+      const [AddressDOM,setAddressDOM]=React.useState('');
+      const [PincodeDOM,setPincodeDOM]=React.useState('');
         const [users,setuser]=useState(users1);
         const [filterval,setfilterval]=useState('')
    
@@ -34,6 +38,32 @@ function Dataentry() {
             setuser(users1)
           }
         }
+        // const deletevalue=(name)=>{
+        //   // setuser(users1.filter(obj => {
+        //   //   return obj.firstname!==(name);
+        //   // }))
+          
+        // }
+    function adddata(){
+      document.getElementById("submitmsg").innerHTML="Submited sucessfully";
+      
+      
+      
+      const newdata=[{firstname:FirstNameDOM,
+        lastname:LastnameDOM,
+        dob:DateDOM,
+        email:EmailDOM,
+        gender:MaleDOM,
+        country:CountryDOM,
+        state:StateDOM,
+        city:CityDOM,
+        address:AddressDOM,
+        pincode:PincodeDOM}]
+        
+        setuser([...users1,...newdata].sort((a,b)=>a.firstname>b.firstname?1:-1,))
+    }
+      
+
     
         
     
@@ -50,18 +80,10 @@ function Dataentry() {
     const displayUser =users
     .slice(pageVisited,pageVisited+userPerPage)
     .map((user)=>{
+   
         return (
             <div className="user">
-                        {/* <h3>{user.firstname}</h3>
-                        <h3>{user.lastname}</h3>
-                        <h3>{user.dob}</h3>
-                        <h3>{user.email}</h3>
-                        <h3>{user.gender}</h3>
-                        <h3>{user.country}</h3>
-                        <h3>{user.state}</h3>
-                        <h3>{user.city}</h3>
-                        <h3>{user.address}</h3>
-                        <h3>{user.pincode}</h3> */}
+                 
                 <table >
                     <tr>
                         <td><input type="checkbox"></input></td>
@@ -75,7 +97,7 @@ function Dataentry() {
                         <td>{user.city}</td>
                         <td>{user.address}</td>
                         <td>{user.pincode}</td>
-                        <td> <label id="edit"><BsFillPencilFill/></label></td>
+                        <td> <label id="edit" ><BsFillPencilFill /></label></td>
                         <td> <label id="clear1"><BsFillTrashFill/></label></td>
                     </tr>
                 </table>
@@ -92,10 +114,10 @@ function Dataentry() {
             <label>Angular 12 CRUD Example with Web API</label>
           </div>
         <div className="container1">
-            <input type='text' id="firstname"  placeholder="FirstName"></input>
-            <input type='text' id="lastname" placeholder="LastName"></input>
-            <input type='date' id="date" placeholder="FirstName"></input>
-            <input type='email' id="email" placeholder="FirstName"></input>
+            <input type='text' id="firstname"  placeholder="FirstName" onChange={e => setFirstNameDOM(e.target.value)}></input>
+            <input type='text' id="lastname" placeholder="LastName" onChange={e => setLastnameDOM(e.target.value)}></input>
+            <input type='date' id="date" onChange={e => setDateDOM(e.target.value)}></input>
+            <input type='email' id="email" placeholder="Email" onChange={e => setEmailDOM(e.target.value)}></input>
             <div className="genderclass">
             <label id='gender'>Gender:</label>
             <br></br>
@@ -108,35 +130,36 @@ function Dataentry() {
             {/* <input type='radio' id="others" value="others"></input>
             <label>Others</label> */}
             </div><br></br>
-            <select name="country" id="country" placeholder='Country'>
+            <select name="country" id="country" placeholder='Country' onChange={e => setCountryDOM(e.target.value)}>
             <option >Country</option>
             <option value="India">India</option>
             <option value="Albania">Albania</option>
             <option value="Andorra">Andorra</option>
             <option value="Haiti">Haiti</option>
             </select>
-            <select name="state" id="state" placeholder='State'>
+            <select name="state" id="state" placeholder='State' onChange={e => serStateDOM(e.target.value)}>
             <option >State</option>
             <option value="Tamilnadu">tamilnadu</option>
             <option value="Andhra">Andhra</option>
             <option value="Delhi">Delhi</option>
             <option value="Goa">Goa</option>
             </select>
-            <select name="city" id="city" placeholder='city'>
+            <select name="city" id="city" placeholder='city'onChange={e => setCityDOM(e.target.value)}>
             <option >City</option>
             <option value="Vellore">Vellore</option>
             <option value="Ranipet">Ranipet</option>
             <option value="Amboor">Amboor</option>
             <option value="Gindi">Gindi</option>
             </select>
-            <input type='text' id="address"  placeholder="Address"></input>
-            <input type='number' id="fpincode"  placeholder="Pincode"></input>
+            <input type='text' id="address"  placeholder="Address" onChange={e => setAddressDOM(e.target.value)}></input>
+            <input type='number' id="fpincode"  placeholder="Pincode"onChange={e => setPincodeDOM(e.target.value)}></input>
 
         </div>
         <div className='container2'>
-            <input type='button' value="Submit" id='submit'></input>
+            <input type='button' value="Submit" id='submit' onClick={adddata}></input>
             <input type='button' value='Reset' id='reset'></input>
         </div>
+        <span id='submitmsg'></span>
        
         
         <div className="Pagegrid">
